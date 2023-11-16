f5aa0116f040c82019371500612c3cfd83fb8f0a
santhisenan <santhisenanajith@gmail.com>
2023-11-17 07:22:23 +0800

    Update run.sh

--- training/run.sh
+++ training/run.sh
@@ -1,2 +1,3 @@
-CUDA_VISIBLE_DEVICES=$2 nohup python $1 --task Metal --train --seed 2021 --run_id $3 > $3.log &
-#CUDA_VISIBLE_DEVICES=$2 python $1 --task Metal --test --seed 2021 --run_id $3
+CUDA_VISIBLE_DEVICES=$2 python $1 --task Metal --train --seed 2021 --run_id $3 > $3.log
+# CUDA_VISIBLE_DEVICES=$2 nohup python $1 --task Metal --train --seed 2021 --run_id $3 > $3.log &
+# CUDA_VISIBLE_DEVICES=$2 python $1 --task Metal --test --seed 2021 --run_id $3
