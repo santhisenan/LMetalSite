9efc189ae5d06c266d02d7d5e8b0ddaa6be3f65b
santhisenan <santhisenanajith@gmail.com>
2023-11-17 10:40:42 +0800

    Add logs to process prottrans

--- training/Feature/ProtTrans/process_ProtTrans.py
+++ training/Feature/ProtTrans/process_ProtTrans.py
@@ -16,7 +16,10 @@ all_protrans = {}
 with open("../../Dataset/Metal_train.pkl", "rb") as f:
     metal_train = pickle.load(f)
 
+
+print("Processing training dataset")
 for ID in metal_train:
+    print(f"Processing for {ID=}")
     raw_protrans = np.load(raw_protrans_path + ID + ".npy")
     Max_protrans.append(np.max(raw_protrans, axis=0))
     Min_protrans.append(np.min(raw_protrans, axis=0))
@@ -24,11 +27,12 @@ for ID in metal_train:
 
 
 ########## Test ##########
-
 with open("../../Dataset/Metal_test.pkl", "rb") as f:
     metal_test = pickle.load(f)
 
+print("Processing testing dataset")
 for ID in metal_test:
+    print(f"Processing for {ID=}")
     raw_protrans = np.load(raw_protrans_path + ID + ".npy")
     all_protrans[ID] = raw_protrans
 
@@ -40,6 +44,8 @@ Min_protrans = np.min(np.array(Min_protrans), axis=0)
 np.save("Max_ProtTrans_repr", Max_protrans)
 np.save("Min_ProtTrans_repr", Min_protrans)
 
+print("Normalising features..")
 for ID in all_protrans:
+    print(f"Processing for {ID=}")
     protrans = (all_protrans[ID] - Min_protrans) / (Max_protrans - Min_protrans)
     np.save(protrans_output_path + ID, protrans)
